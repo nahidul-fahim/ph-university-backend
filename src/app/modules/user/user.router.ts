936868251f8317180bfb53833d036b705f42db9f
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-27 23:08:38 +0600

    created validation middleware

--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { UserControllers } from "./user.controller";
+import { createStudentValidationSchema } from "../student/student.validation";
+import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
 // create student route
-router.post("/create-student", UserControllers.createStudent)
+router.post("/create-student", validateRequest(createStudentValidationSchema), UserControllers.createStudent)
 
 
 export const UserRoutes = router;
