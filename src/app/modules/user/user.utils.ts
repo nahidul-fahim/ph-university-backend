de449afd3a7401fc17146a79006efb67c2dd100a
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-30 23:09:40 +0600

    created new id for new semester and year

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -17,13 +17,28 @@ const findLastStudentId = async () => {
         })
         .lean()
 
-    return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 
 // create student id
 export const generateStudentId = async (payload: TAcademicSemester) => {
-    const currentId = (await findLastStudentId()) || (0).toString();
+    let currentId = (0).toString(); // by default 0000
+
+    const lastStudentId = await findLastStudentId();
+    // last student id example: 2030 01 0001
+    const lastSemesterCode = lastStudentId?.substring(4, 6); // 01
+    const lastStudentYear = lastStudentId?.substring(0, 4) // 2030
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if (lastStudentId && lastSemesterCode === currentSemesterCode && lastStudentYear === currentYear) {
+        currentId = lastStudentId.substring(6); // this will add with 0001
+    }
+
+
+
+
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0'); // creating 4 digits according to serial
     incrementId = `${payload.year}${payload.code}${incrementId}`; // creating the full id
     return incrementId;
