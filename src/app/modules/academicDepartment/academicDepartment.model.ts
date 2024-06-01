f11eae399e8eb933fe833bac02c6f18d1936f47d
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-06-01 23:10:48 +0600

    created validation for department creationa and update department data

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -19,5 +19,28 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     }
 )
 
+// checking if academic department already exists
+academicDepartmentSchema.pre('save', async function (next) {
+    const isDepartmentExist = await AcademicDepartment.findOne({
+        name: this.name
+    })
+    if (isDepartmentExist) {
+        throw new Error("This department is already exists!")
+    }
+    next();
+})
+
+
+// checking if academic department exists to update data
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+    const query = this.getQuery();
+    const isDepartmentExist = await AcademicDepartment.findOne(query)
+
+    if (!isDepartmentExist) {
+        throw new Error("This department doesn't exist.")
+    }
+    next();
+})
+
 
 export const AcademicDepartment = model<TAcademicDepartment>('AcademicDepartment', academicDepartmentSchema)
