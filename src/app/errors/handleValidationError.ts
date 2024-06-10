825ca6e9546add054f42c050e431970339d9c894
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-06-11 00:29:13 +0600

    handle error validation

--- src/app/errors/handleValidationError.ts
+++ src/app/errors/handleValidationError.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import { TErrorSources } from "../interface/error";
+import { TErrorSources, TGenericErrorResponse } from "../interface/error";
 
 // handle validation error
-const handleValidationError = (err: mongoose.Error.ValidationError) => {
+const handleValidationError = (err: mongoose.Error.ValidationError): TGenericErrorResponse => {
     const statusCode = 400;
     const errorSources: TErrorSources = Object.values(err.errors).map((val: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
         return {
